253ac21cb4038839c243c586ef444a049aa05677
XParliamentX <54242544+XParliamentX@users.noreply.github.com>
2024-05-03 04:19:40 +0700

    Add files via upload

---
+++ KG_LAB_III/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace KG_LAB_III
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonDrawAxes = new System.Windows.Forms.Button();
+            this.buttonDrawFigure = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.labelSlide = new System.Windows.Forms.Label();
+            this.buttonSlideOxRt = new System.Windows.Forms.Button();
+            this.buttonSlideOxLt = new System.Windows.Forms.Button();
+            this.buttonSlideOyDown = new System.Windows.Forms.Button();
+            this.buttonSlideOyUp = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonReflectOXandOY = new System.Windows.Forms.Button();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.labelScale = new System.Windows.Forms.Label();
+            this.buttonTurnLt = new System.Windows.Forms.Button();
+            this.buttonTurnRt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelForEx3 = new System.Windows.Forms.Label();
+            this.labelReflect = new System.Windows.Forms.Label();
+            this.buttonReflectOX = new System.Windows.Forms.Button();
+            this.buttonReflectOY = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(625, 625);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonDrawAxes
+            // 
+            this.buttonDrawAxes.Location = new System.Drawing.Point(644, 13);
+            this.buttonDrawAxes.Name = "buttonDrawAxes";
+            this.buttonDrawAxes.Size = new System.Drawing.Size(196, 54);
+            this.buttonDrawAxes.TabIndex = 1;
+            this.buttonDrawAxes.Text = "Нарисовать Оси";
+            this.buttonDrawAxes.UseVisualStyleBackColor = true;
+            this.buttonDrawAxes.Click += new System.EventHandler(this.buttonDrawAxes_Click);
+            // 
+            // buttonDrawFigure
+            // 
+            this.buttonDrawFigure.Location = new System.Drawing.Point(643, 73);
+            this.buttonDrawFigure.Name = "buttonDrawFigure";
+            this.buttonDrawFigure.Size = new System.Drawing.Size(196, 54);
+            this.buttonDrawFigure.TabIndex = 2;
+            this.buttonDrawFigure.Text = "Нарисовать Фигуру";
+            this.buttonDrawFigure.UseVisualStyleBackColor = true;
+            this.buttonDrawFigure.Click += new System.EventHandler(this.buttonDrawFigure_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(644, 133);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(196, 54);
+            this.buttonClear.TabIndex = 3;
+            this.buttonClear.Text = "Очистить";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // labelSlide
+            // 
+            this.labelSlide.AutoSize = true;
+            this.labelSlide.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelSlide.Location = new System.Drawing.Point(675, 233);
+            this.labelSlide.Name = "labelSlide";
+            this.labelSlide.Size = new System.Drawing.Size(128, 54);
+            this.labelSlide.TabIndex = 4;
+            this.labelSlide.Text = "Сдвиг";
+            this.labelSlide.UseCompatibleTextRendering = true;
+            // 
+            // buttonSlideOxRt
+            // 
+            this.buttonSlideOxRt.Location = new System.Drawing.Point(643, 290);
+            this.buttonSlideOxRt.Name = "buttonSlideOxRt";
+            this.buttonSlideOxRt.Size = new System.Drawing.Size(196, 54);
+            this.buttonSlideOxRt.TabIndex = 5;
+            this.buttonSlideOxRt.Text = "По оси OX вправо";
+            this.buttonSlideOxRt.UseVisualStyleBackColor = true;
+            this.buttonSlideOxRt.Click += new System.EventHandler(this.buttonSlideOxRt_Click);
+            // 
+            // buttonSlideOxLt
+            // 
+            this.buttonSlideOxLt.Location = new System.Drawing.Point(643, 350);
+            this.buttonSlideOxLt.Name = "buttonSlideOxLt";
+            this.buttonSlideOxLt.Size = new System.Drawing.Size(196, 54);
+            this.buttonSlideOxLt.TabIndex = 6;
+            this.buttonSlideOxLt.Text = "По оси OX влево";
+            this.buttonSlideOxLt.UseVisualStyleBackColor = true;
+            this.buttonSlideOxLt.Click += new System.EventHandler(this.buttonSlideOxLt_Click);
+            // 
+            // buttonSlideOyDown
+            // 
+            this.buttonSlideOyDown.Location = new System.Drawing.Point(643, 410);
+            this.buttonSlideOyDown.Name = "buttonSlideOyDown";
+            this.buttonSlideOyDown.Size = new System.Drawing.Size(196, 54);
+            this.buttonSlideOyDown.TabIndex = 7;
+            this.buttonSlideOyDown.Text = "По оси OY вниз";
+            this.buttonSlideOyDown.UseVisualStyleBackColor = true;
+            this.buttonSlideOyDown.Click += new System.EventHandler(this.buttonSlideOyDown_Click);
+            // 
+            // buttonSlideOyUp
+            // 
+            this.buttonSlideOyUp.Location = new System.Drawing.Point(644, 470);
+            this.buttonSlideOyUp.Name = "buttonSlideOyUp";
+            this.buttonSlideOyUp.Size = new System.Drawing.Size(196, 54);
+            this.buttonSlideOyUp.TabIndex = 8;
+            this.buttonSlideOyUp.Text = "По оси OY вверх";
+            this.buttonSlideOyUp.UseVisualStyleBackColor = true;
+            this.buttonSlideOyUp.Click += new System.EventHandler(this.buttonSlideOyUp_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(643, 583);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(196, 54);
+            this.buttonStart.TabIndex = 9;
+            this.buttonStart.Text = "Старт";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonReflectOXandOY
+            // 
+            this.buttonReflectOXandOY.Location = new System.Drawing.Point(877, 253);
+            this.buttonReflectOXandOY.Name = "buttonReflectOXandOY";
+            this.buttonReflectOXandOY.Size = new System.Drawing.Size(196, 54);
+            this.buttonReflectOXandOY.TabIndex = 10;
+            this.buttonReflectOXandOY.Text = "Отражение по OX и OY";
+            this.buttonReflectOXandOY.UseVisualStyleBackColor = true;
+            this.buttonReflectOXandOY.Click += new System.EventHandler(this.buttonReflect_Click);
+            // 
+            // buttonPlus
+            // 
+            this.buttonPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonPlus.Location = new System.Drawing.Point(877, 350);
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.Size = new System.Drawing.Size(99, 54);
+            this.buttonPlus.TabIndex = 13;
+            this.buttonPlus.Text = "+";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.buttonPlus_Click);
+            // 
+            // buttonMinus
+            // 
+            this.buttonMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonMinus.Location = new System.Drawing.Point(974, 350);
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.Size = new System.Drawing.Size(99, 54);
+            this.buttonMinus.TabIndex = 14;
+            this.buttonMinus.Text = "-";
+            this.buttonMinus.UseVisualStyleBackColor = true;
+            this.buttonMinus.Click += new System.EventHandler(this.buttonMinus_Click);
+            // 
+            // labelScale
+            // 
+            this.labelScale.AutoSize = true;
+            this.labelScale.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelScale.Location = new System.Drawing.Point(876, 310);
+            this.labelScale.Name = "labelScale";
+            this.labelScale.Size = new System.Drawing.Size(187, 29);
+            this.labelScale.TabIndex = 15;
+            this.labelScale.Text = "Масштабирование";
+            this.labelScale.UseCompatibleTextRendering = true;
+            // 
+            // buttonTurnLt
+            // 
+            this.buttonTurnLt.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonTurnLt.Location = new System.Drawing.Point(876, 467);
+            this.buttonTurnLt.Name = "buttonTurnLt";
+            this.buttonTurnLt.Size = new System.Drawing.Size(99, 54);
+            this.buttonTurnLt.TabIndex = 16;
+            this.buttonTurnLt.Text = "Налево";
+            this.buttonTurnLt.UseVisualStyleBackColor = true;
+            this.buttonTurnLt.Click += new System.EventHandler(this.buttonTurnLt_Click);
+            // 
+            // buttonTurnRt
+            // 
+            this.buttonTurnRt.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonTurnRt.Location = new System.Drawing.Point(974, 467);
+            this.buttonTurnRt.Name = "buttonTurnRt";
+            this.buttonTurnRt.Size = new System.Drawing.Size(99, 54);
+            this.buttonTurnRt.TabIndex = 17;
+            this.buttonTurnRt.Text = "Направо";
+            this.buttonTurnRt.UseVisualStyleBackColor = true;
+            this.buttonTurnRt.Click += new System.EventHandler(this.buttonTurnRt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(925, 435);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 29);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Поворот";
+            this.label1.UseCompatibleTextRendering = true;
+            // 
+            // labelForEx3
+            // 
+            this.labelForEx3.AutoSize = true;
+            this.labelForEx3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelForEx3.Location = new System.Drawing.Point(876, 28);
+            this.labelForEx3.Name = "labelForEx3";
+            this.labelForEx3.Size = new System.Drawing.Size(207, 39);
+            this.labelForEx3.TabIndex = 19;
+            this.labelForEx3.Text = "Для 3 задания";
+            this.labelForEx3.UseCompatibleTextRendering = true;
+            // 
+            // labelReflect
+            // 
+            this.labelReflect.AutoSize = true;
+            this.labelReflect.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelReflect.Location = new System.Drawing.Point(899, 95);
+            this.labelReflect.Name = "labelReflect";
+            this.labelReflect.Size = new System.Drawing.Size(165, 32);
+            this.labelReflect.TabIndex = 20;
+            this.labelReflect.Text = "Отражение";
+            // 
+            // buttonReflectOX
+            // 
+            this.buttonReflectOX.Location = new System.Drawing.Point(877, 133);
+            this.buttonReflectOX.Name = "buttonReflectOX";
+            this.buttonReflectOX.Size = new System.Drawing.Size(196, 54);
+            this.buttonReflectOX.TabIndex = 21;
+            this.buttonReflectOX.Text = "Отражение по OX";
+            this.buttonReflectOX.UseVisualStyleBackColor = true;
+            this.buttonReflectOX.Click += new System.EventHandler(this.buttonReflectOX_Click);
+            // 
+            // buttonReflectOY
+            // 
+            this.buttonReflectOY.Location = new System.Drawing.Point(876, 193);
+            this.buttonReflectOY.Name = "buttonReflectOY";
+            this.buttonReflectOY.Size = new System.Drawing.Size(196, 54);
+            this.buttonReflectOY.TabIndex = 22;
+            this.buttonReflectOY.Text = "Отражение по OY";
+            this.buttonReflectOY.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 649);
+            this.Controls.Add(this.buttonReflectOY);
+            this.Controls.Add(this.buttonReflectOX);
+            this.Controls.Add(this.labelReflect);
+            this.Controls.Add(this.labelForEx3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonTurnRt);
+            this.Controls.Add(this.buttonTurnLt);
+            this.Controls.Add(this.labelScale);
+            this.Controls.Add(this.buttonMinus);
+            this.Controls.Add(this.buttonPlus);
+            this.Controls.Add(this.buttonReflectOXandOY);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonSlideOyUp);
+            this.Controls.Add(this.buttonSlideOyDown);
+            this.Controls.Add(this.buttonSlideOxLt);
+            this.Controls.Add(this.buttonSlideOxRt);
+            this.Controls.Add(this.labelSlide);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonDrawFigure);
+            this.Controls.Add(this.buttonDrawAxes);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonDrawAxes;
+        private System.Windows.Forms.Button buttonDrawFigure;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Label labelSlide;
+        private System.Windows.Forms.Button buttonSlideOxRt;
+        private System.Windows.Forms.Button buttonSlideOxLt;
+        private System.Windows.Forms.Button buttonSlideOyDown;
+        private System.Windows.Forms.Button buttonSlideOyUp;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonReflectOXandOY;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.Label labelScale;
+        private System.Windows.Forms.Button buttonTurnLt;
+        private System.Windows.Forms.Button buttonTurnRt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelForEx3;
+        private System.Windows.Forms.Label labelReflect;
+        private System.Windows.Forms.Button buttonReflectOX;
+        private System.Windows.Forms.Button buttonReflectOY;
+    }
+}
+
---
+++ KG_LAB_III/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KG_LAB_III
+{
+    public partial class Form1 : Form
+    {
+
+        int[,] kv = new int[4, 3]; // матрица тела
+        int[,] osi = new int[4, 3]; // матрица координат осей
+        int[,] matr_sdv = new int[3, 3]; //матрица преобразования
+        int k, l; // элементы матрицы сдвига
+        bool f = true;
+        int[,] T90 = new int[2, 3];
+        int[,] T180 = new int[2, 3];
+        int[,] T270 = new int[2, 3];
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Init_T90()
+        {
+            T90[0,0] = 0; T90[0,1] = 1; T90[0, 2] = 1;
+            T90[1,0] = -1; T90[1, 1] = 0; T90[1, 2] = 1;
+        }
+        private void Init_T180()
+        {
+            T180[0, 0] = -1; T180[0, 1] = 0; T180[0, 2] = 1;
+            T180[1, 0] = 0; T180[1, 1] = -1; T180[1, 2] = 1;
+        }
+        private void Init_T270()
+        {
+            T270[0, 0] = 0; T270[0, 1] = -1; T270[0, 2] = 1;
+            T270[1, 0] = 1; T270[1, 1] = 0; T270[1, 2] = 1;
+        }
+        //инициализация матрицы тела
+        private void Init_kvadrat()
+        {
+            kv[0, 0] = -40; kv[0, 1] = 50; kv[0, 2] = 1;
+            kv[1, 0] = 40; kv[1, 1] = -30; kv[1, 2] = 1;
+            kv[2, 0] = 38; kv[2, 1] = 10; kv[2, 2] = 1;
+            kv[3, 0] = -50; kv[3, 1] = -50; kv[3, 2] = 1;
+        }
+
+        //инициализация матрицы сдвига
+        private void Init_matr_preob(int k1, int l1)
+        {
+            matr_sdv[0, 0] = 1; matr_sdv[0, 1] = 0; matr_sdv[0, 2] = 0;
+            matr_sdv[1, 0] = 0; matr_sdv[1, 1] = 1; matr_sdv[1, 2] = 0;
+            matr_sdv[2, 0] = k1; matr_sdv[2, 1] = l1; matr_sdv[2, 2] = 1;
+        }
+
+        //инициализация матрицы осей
+        private void Init_osi()
+        {
+            osi[0, 0] = 0; osi[0, 1] = pictureBox1.Height / 2; osi[0, 2] = 1;
+            osi[1, 0] = pictureBox1.Width; osi[1, 1] = pictureBox1.Height / 2; osi[1, 2] = 1;
+            osi[2, 0] = pictureBox1.Width / 2; osi[2, 1] = 0; osi[2, 2] = 1;
+            osi[3, 0] = pictureBox1.Width / 2; osi[3, 1] = pictureBox1.Height; osi[3, 2] = 1;
+        }
+
+        //умножение матриц
+        private int[,] Multiply_matr(int[,] a, int[,] b)
+        {
+            int n = a.GetLength(0);
+            int m = a.GetLength(1);
+
+            int[,] r = new int[n, m];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    r[i, j] = 0;
+                    for (int ii = 0; ii < m; ii++)
+                    {
+                        r[i, j] += a[i, ii] * b[ii, j];
+                    }
+                }
+            }
+            return r;
+        }
+
+        //вывод квадрата на экран
+        private void Draw_Kv()
+        {
+
+            Init_kvadrat(); //инициализация матрицы тела
+            Init_matr_preob(k, l); //инициализация матрицы преобразования
+            int[,] kv1 = Multiply_matr(kv, matr_sdv); //перемножение матриц
+
+            Pen myPen = new Pen(Color.Blue, 2);// цвет линии и ширина
+
+            //создаем новый объект Graphics (поверхность рисования) из pictureBox
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+            // рисуем 1 сторону квадрата
+            g.DrawLine(myPen, kv1[0, 0], kv1[0, 1], kv1[1, 0], kv1[1, 1]);
+            // рисуем 2 сторону квадрата
+            g.DrawLine(myPen, kv1[1, 0], kv1[1, 1], kv1[2, 0], kv1[2, 1]);
+            // рисуем 3 сторону квадрата
+            g.DrawLine(myPen, kv1[2, 0], kv1[2, 1], kv1[3, 0], kv1[3, 1]);
+            // рисуем 4 сторону квадрата
+            g.DrawLine(myPen, kv1[3, 0], kv1[3, 1], kv1[0, 0], kv1[0, 1]);
+            g.Dispose();// освобождаем все ресурсы, связанные с отрисовкой
+            myPen.Dispose(); //освобождвем ресурсы, связанные с Pen
+
+        }
+        //вывод осей на экран
+        private void Draw_osi()
+        {
+            Init_osi(); //инициализация матрицы осей
+            Pen myPen = new Pen(Color.Red, 1);// цвет линии и ширина
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+            // рисуем ось ОХ
+            g.DrawLine(myPen, osi[0, 0], osi[0, 1], osi[1, 0], osi[1, 1]);
+            // рисуем ось ОУ
+            g.DrawLine(myPen, osi[2, 0], osi[2, 1], osi[3, 0], osi[3, 1]);
+            g.Dispose();
+            myPen.Dispose();
+        }
+
+        private void Turn(int angle)
+        {
+            if (angle == 90)
+            {
+                Init_T90();
+                kv = Multiply_matr(kv, T90); // Присваивание результата умножения обратно переменной kv
+                Draw_Kv();
+            }
+            else if (angle == 180)
+            {
+                Init_T180();
+                kv = Multiply_matr(kv, T180); // Присваивание результата умножения обратно переменной kv
+                Draw_Kv();
+            }
+            else if (angle == 270)
+            {
+                Init_T270();
+                kv = Multiply_matr(kv, T270); // Присваивание результата умножения обратно переменной kv
+                Draw_Kv();
+            }
+        }
+
+        private void buttonDrawAxes_Click(object sender, EventArgs e)
+        {
+            k = pictureBox1.Width / 2;
+            l = pictureBox1.Height / 2;
+            Draw_osi();
+        }
+
+        private void buttonDrawFigure_Click(object sender, EventArgs e)
+        {
+            //середина pictureBox
+            k = pictureBox1.Width / 2;
+            l = pictureBox1.Height / 2;
+            //вывод квадратика в середине
+            Draw_Kv();
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+        }
+
+        private void buttonSlideOxRt_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+            Draw_osi();
+            k += 5; //изменение соответствующего элемента матрицы сдвига
+            Draw_Kv(); //вывод квадратика
+        }
+
+        private void buttonSlideOxLt_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+            Draw_osi();
+            k -= 5; //изменение соответствующего элемента матрицы сдвига
+            Draw_Kv(); //вывод квадратика
+        }
+
+        private void buttonSlideOyDown_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+            Draw_osi();
+            l += 5; //изменение соответствующего элемента матрицы сдвига
+            Draw_Kv(); //вывод квадратика
+        }
+
+        private void buttonSlideOyUp_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+            Draw_osi();
+            l -= 5; //изменение соответствующего элемента матрицы сдвига
+            Draw_Kv(); //вывод квадратика
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = 100;
+
+            buttonStart.Text = "Стоп";
+            if (f == true)
+                timer1.Start();
+            else
+            {
+                timer1.Stop();
+                buttonStart.Text = "Старт";
+            }
+            f = !f;
+        }
+
+        private void Scale(int factor)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                kv[i, 0] *= factor;
+                kv[i, 1] *= factor;
+            }
+            Draw_Kv();
+        }
+        private void buttonReflect_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            Scale(2);
+        }
+
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+            Scale(1/2);
+        }
+
+        private void buttonTurnLt_Click(object sender, EventArgs e)
+        {
+            Turn(90);
+        }
+
+        private void buttonTurnRt_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonReflectOX_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            k++;
+            Draw_Kv();
+            Thread.Sleep(100);
+
+        }
+
+        
+    }
+}
